9f63d994caaf8b15d2a741a8995e8364a2c851b4
DipRabadiya27 <diprabadiya@gmail.com>
2023-09-14 19:25:10 +0530

    add

---
+++ Recursion/r6.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+//print sum of first N number
+public class r6 {
+    // parameter 
+    // static void printSum(int i,int sum) {
+    //     if(i < 1){
+    //         System.out.println(sum);
+    //         return;
+    //     }
+    //     printSum(i-1,sum+i);
+    // }
+
+    //function 
+    static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int sum=0;
+        // printSum(n,sum);
+        System.out.println(sum(n));
+        
+    }
+}
---
+++ Recursion/r7.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+//print factorial of n
+public class r7 {
+    // parameter 
+    // static void printFactorial(int i,int multiply) {
+    //     if(i < 1){
+    //         System.out.println(multiply);
+    //         return;
+    //     }
+    //     printFactorial(i-1,multiply*i);
+    // }
+
+    // function 
+    static int printFactorial(int n){
+        if(n==0){
+            return 1;
+        }
+        return n * printFactorial(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int factorial=1;
+        // printFactorial(n,factorial);
+        System.out.println(printFactorial(n));
+        
+    }
+}
