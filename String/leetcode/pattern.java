75aa1eb3e87b1ab965778cd32f3fbbe718803bce
DipRabadiya27 <diprabadiya@gmail.com>
2023-09-21 14:56:44 +0530

    add

---
+++ String/leetcode/pattern.java
@@ -0,0 +1,24 @@
+package String.leetcode;
+
+public class pattern {
+    public static void main(String[] args) {
+        int a=5;
+        int x=1;
+        for(int i=0;i<a;i++){
+            int y=x;
+            for(int j=0;j<a;j++){
+                if(j<=i){
+                    if(i%2==0){
+                        System.out.print(y +" ");
+                        y++;
+                    }else{
+                        System.out.print(y +" ");
+                        y--;
+                    }
+                }
+            }
+            System.out.println();
+            x+=2;
+    }
+    }
+}
