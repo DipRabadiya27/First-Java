7d2518455dabe66a825bbca164c2696c13ebb36b
DipRabadiya27 <diprabadiya@gmail.com>
2023-10-05 12:17:19 +0530

    add

--- Stack/get_min.java
+++ Stack/get_min.java
@@ -1,17 +1,40 @@
 package Stack;
-import java.util.*;
-
+import java.util.Stack;
+//02-08-2023
+//Find min in O(1) from stack.
 public class get_min {
+    static int min = Integer.MAX_VALUE;
+    static Stack<Integer> stack = new Stack<>();
+
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(10);
-        stack.push(50);
-        stack.push(8);
-        stack.push(20);
-        stack.push(100);
-        stack.push(77);
-        
+        push(10);
+        push(20);
+        pop();
+        push(45);
+        push(5);
+        pop();
+        System.out.println(getMin());
+    }
+    
+    public static void push(int val) {
+        if(val <= min){
+            stack.add(min);
+            min = val;
+        }
+        stack.add(val);
+    }
 
+    public static void pop() {
+        if(stack.pop() == min){
+            min = stack.pop();
+        }
+    }
 
+    // public static int top() {
+    //     return stack.peek();
+    // }
+
+    public static int getMin() {
+        return min;
     }
 }
